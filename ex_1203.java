ad8b61a706fcbed3eeb1bd522a4c677115fe13a9
Шалупин Данил <70798467+SD1999B52@users.noreply.github.com>
2021-03-22 19:11:19 +0500

    Add files via upload

--- ex_1203.java
+++ ex_1203.java
@@ -53,8 +53,7 @@ class ex_1203 {
 	}
 	
 	public static void getMaxNumOfReports( int[][] time ) {
-		ArrayList<Integer> timeA = new ArrayList<Integer>();
-		ArrayList<Integer> timeB = new ArrayList<Integer>();
+		ArrayList<int[]> timeS = new ArrayList<int[]>();
 		
 		//находим минимальное значение для первой конференции
 		int min = time[0][1];
@@ -66,42 +65,39 @@ class ex_1203 {
 		
 		while ( 1 < 2 ) {
 			//заполняем ArrayList A и B интервалами начало которых на 1 больше последнего записанного
-			ArrayList<Integer> minA = new ArrayList<Integer>();
-			ArrayList<Integer> minB = new ArrayList<Integer>();
+			ArrayList<int[]> minS = new ArrayList<int[]>();
+			
 			for ( int i = 0; i < time.length; i++ ) {
-				if ( timeA.size() == 0 ) {
+				if ( timeS.size() == 0 ) {
 					if ( time[i][1] == min ) {
-						minA.add( time[i][0] );
-						minB.add( time[i][1] );
+						minS.add( new int[] { time[i][0], time[i][1] } );
 					}
 				} else {
-					if ( time[i][0] - timeB.get( timeB.size() - 1 ) >= 1 ) {
-						minA.add( time[i][0] );
-						minB.add( time[i][1] );
+					if ( time[i][0] - timeS.get( timeS.size() - 1 )[1] >= 1 ) {
+						minS.add( new int[] { time[i][0], time[i][1] } );
 					}
 				}
 			}
 			
 			//находим наименьший интервал
 			int index = 0;
-			for ( int i = 0; i < minB.size(); i++ ) {
-				if ( minB.get( i ) < minB.get( index )) {
+			for ( int i = 0; i < minS.size(); i++ ) {
+				if ( minS.get( i )[1] < minS.get( index )[1] ) {
 					index = i;
 				}
 			}
 			
 			//завершаем поиск если нет больше вариантов
-			if ( minB.size() == 0 ) {
+			if ( minS.size() == 0 ) {
 				break;
 			}
 			
 			//добавляем интервал
-			timeA.add( minA.get( index ));
-			timeB.add( minB.get( index ));
+			timeS.add( new int[] { minS.get( index )[0], minS.get( index )[1] } );
 			
-			System.out.print( minA.get( index ) + "-" + minB.get( index ) + " " );
+			System.out.print( minS.get( index )[0] + "-" + minS.get( index )[1] + " " );
 		}
 		
-		System.out.println( "\nMaximum number of reports: " + timeA.size());
+		System.out.println( "\nMaximum number of reports: " + timeS.size());
 	}
 }
